fe6179dd0eb0a5a7e232a84497d7b3ca21b6f98f
Krista Shuckerow <kshuckerow@gmail.com>
2017-10-08 12:55:47 -0400

    remove dart file

--- main.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:html';
-
-main() async {
-  var countdown = querySelector("#countdown");
-  for (int i = 100; i >= 0; i--) {
-    countdown.text = "Time: $i";
-    await window.animationFrame;
-  }
-}
